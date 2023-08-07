3d1ca1a183b4a83fa152669c175684fe7c6d20a2
MarcialFantini <marcialfantini@gmail.com>
2023-08-07 20:11:09 -0300

    arreglando git

--- src/libs/pg.ts
+++ src/libs/pg.ts
@@ -2,10 +2,11 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   host: "localhost",
-  user: "postgres",
+  user: "marcial",
   database: "ecommerce",
-  password: "marcial",
+  password: "hola123",
   max: 40,
   idle_in_transaction_session_timeout: 30000,
   connectionTimeoutMillis: 2000,
+  port: 5332,
 });
